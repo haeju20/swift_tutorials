5ae1b5777ddd9da3ab4ec529d3a639392498cc17
jisu <1712wltn@gmail.com>
2022-03-14 18:17:41 +0900

    Initial Commit

---
+++ test01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test01
+//
+//  Created by 박지수 on 3/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test01/test01App.swift
@@ -0,0 +1,17 @@
+//
+//  test01App.swift
+//  test01
+//
+//  Created by 박지수 on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct test01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

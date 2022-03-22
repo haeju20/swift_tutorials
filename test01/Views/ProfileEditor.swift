bc9d581d67f7c5611d0e1a19f8a17882dfe59055
jisu <1712wltn@gmail.com>
2022-03-22 23:56:38 +0900

    Swift Tutorials - Working with UI Controls (2/5) (ProfileEditor.swift)

---
+++ test01/Views/ProfileEditor.swift
@@ -0,0 +1,43 @@
+//
+//  ProfileEditor.swift
+//  test01
+//
+//  Created by 박지수 on 3/22/22.
+//
+
+import SwiftUI
+
+struct ProfileEditor: View {
+    @Binding var profile: Profile
+    
+    var body: some View {
+        List {
+            HStack {
+                Text("Username").bold()
+                Divider()
+                TextField("Username", text: $profile.username)
+            }
+            
+            Toggle(isOn: $profile.prefersNotifications) {
+                Text("Enable Notifications").bold()
+            }
+            
+            VStack(alignment: .leading, spacing: 20) {
+                Text("Seasonal Photo").bold()
+                
+                Picker("Seasonal Photo", selection: $profile.seasonalPhoto) {
+                    ForEach(Profile.Season.allCases) { season in
+                        Text(season.rawValue).tag(season)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+        }
+    }
+}
+
+struct ProfileEditor_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileEditor(profile: .constant(.default))
+    }
+}
